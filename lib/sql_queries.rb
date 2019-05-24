0f899aa53f4922fc1edc2a4b6f6af01f9cd0e3a5
Kishandth Sivapalasundaram <rodes88@msn.com>
2019-05-24 15:47:11 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -29,7 +29,7 @@ def selects_the_titles_and_amount_over_goal_of_all_projects_that_have_met_their_
 FROM projects a 
 LEFT JOIN pledges b 
   ON a.id = b.project_id 
-GROUP BY id
+GROUP BY a.id
 HAVING SUM(amount)
 ORDER BY SUM(amount)"
 end
