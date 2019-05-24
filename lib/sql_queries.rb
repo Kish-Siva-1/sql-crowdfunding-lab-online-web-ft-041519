a6231da6b805b33f34c7f52da6169f34b0850ffa
Kishandth Sivapalasundaram <rodes88@msn.com>
2019-05-24 16:24:05 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -53,5 +53,9 @@ WHERE a.category LIKE '%music%'"
 end
 
 def selects_the_category_name_and_the_sum_total_of_the_all_its_pledges_for_the_books_category
-"Write your SQL query Here"
+"SELECT category, SUM(amount)
+FROM projects a 
+INNER JOIN pledges b 
+  ON a.id = b.project_id 
+GROUP BY books"
 end
