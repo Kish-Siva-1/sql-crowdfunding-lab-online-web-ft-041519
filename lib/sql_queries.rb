6b4bb13998be31eca1e19ac000efaa209668f51c
Kishandth Sivapalasundaram <rodes88@msn.com>
2019-05-24 15:47:44 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -25,7 +25,7 @@ ORDER BY name"
 end
 
 def selects_the_titles_and_amount_over_goal_of_all_projects_that_have_met_their_funding_goal
-"SELECT title, SUM(amount), funding_goal
+"SELECT title, SUM(amount) - funding_goal
 FROM projects a 
 LEFT JOIN pledges b 
   ON a.id = b.project_id 
