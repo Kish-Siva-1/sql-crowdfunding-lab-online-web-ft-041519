48973a4198d267152fb622c4ef48db7a77a99faa
Kishandth Sivapalasundaram <rodes88@msn.com>
2019-05-24 16:04:14 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -44,7 +44,13 @@ ORDER BY SUM(amount), name"
 end
 
 def selects_the_category_names_and_pledge_amounts_of_all_pledges_in_the_music_category
-"Write your SQL query Here"
+"SELECT name, SUM(amount)
+FROM users a 
+LEFT JOIN pledges b 
+  ON a.id = b.user_id 
+GROUP BY name
+WHERE 
+ORDER BY SUM(amount), name"
 end
 
 def selects_the_category_name_and_the_sum_total_of_the_all_its_pledges_for_the_books_category
