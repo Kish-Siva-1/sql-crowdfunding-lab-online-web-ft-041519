b9a3915e14aaa6ec1a3237c5bd3467f884dc2946
Kishandth Sivapalasundaram <rodes88@msn.com>
2019-05-24 14:41:50 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -7,7 +7,7 @@
 # Make sure each ruby method returns a string containing a valid SQL statement.
 
 def selects_the_titles_of_all_projects_and_their_pledge_amounts_alphabetized_by_name
-"SELECT title, SUM(a.amount)
+"SELECT title, SUM(amount)
 FROM projects a 
 LEFT JOIN pledges b 
   ON a.id = b.project_id 
