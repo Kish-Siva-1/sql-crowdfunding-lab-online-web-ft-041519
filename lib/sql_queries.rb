ba3c3f13a383932d286924e64b1ba2481ed187c5
Kishandth Sivapalasundaram <rodes88@msn.com>
2019-05-24 16:05:30 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -44,13 +44,12 @@ ORDER BY SUM(amount), name"
 end
 
 def selects_the_category_names_and_pledge_amounts_of_all_pledges_in_the_music_category
-"SELECT name, SUM(amount)
-FROM users a 
+"SELECT name, category, SUM(amount)
+FROM projects a 
 LEFT JOIN pledges b 
-  ON a.id = b.user_id 
-GROUP BY name
-WHERE 
-ORDER BY SUM(amount), name"
+  ON a.id = b.project_id 
+WHERE category = music
+GROUP BY title"
 end
 
 def selects_the_category_name_and_the_sum_total_of_the_all_its_pledges_for_the_books_category
